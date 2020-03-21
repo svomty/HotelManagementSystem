19faf559df849bb64d401a28c28c7e9cc87d800c
Artyom Svintsitski <svomty@mail.ru>
2020-03-21 20:13:39 +0300

    Указал в логировании ip адрес

--- src/main/java/com/svintsitski/hotel_management_system/MainController.java
+++ src/main/java/com/svintsitski/hotel_management_system/MainController.java
@@ -4,7 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @Controller
@@ -13,8 +16,9 @@ public class MainController {
     private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
     @RequestMapping("/")
-    public String welcome() {
-        LOGGER.info("return index");
+    public String welcome(HttpServletRequest request) {
+        String ip = request.getRemoteAddr();
+        LOGGER.info("[" + ip + "] requested index.jsp");
         return "index";
     }
 
