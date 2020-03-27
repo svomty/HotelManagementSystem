f3fa0e074aa9120dc4e66fe243fe0bec68e3bc72
Artyom Svintsitski <svomty@mail.ru>
2020-03-27 22:54:59 +0300

    Логирование страницы ошибок

--- src/main/java/com/svintsitski/hotel_management_system/controller/ErrorController.java
+++ src/main/java/com/svintsitski/hotel_management_system/controller/ErrorController.java
@@ -1,13 +1,31 @@
 package com.svintsitski.hotel_management_system.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ErrorController.class);
+
     @RequestMapping("/error")
-    public String handleError() {
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        String url = request.getServletPath();
+        String ip = request.getRemoteAddr();
+
+        String info = "[" + ip + "] requested " + url;
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            LOGGER.error(info + "; status code = " + statusCode.toString());
+        }
         return "error";
     }
 
