a6b732f76cc199426a7ab50aa8d12b3c57bb012a
Artyom Svintsitski <svomty@mail.ru>
2020-03-31 21:04:14 +0300

    Разработка ApartmentController . findAll()

--- src/main/java/com/svintsitski/hotel_management_system/model/Apartment.java
+++ src/main/java/com/svintsitski/hotel_management_system/model/Apartment.java
@@ -24,6 +24,10 @@ public class Apartment {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public byte getNumber() {
         return number;
     }
